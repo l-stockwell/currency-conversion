7c3395cc68f9e8ec14ec266df6fbb7d77422f168
l-stockwell <lena.stockwell@gmail.com>
2025-03-24 16:21:59 +1100

    Modify Dropdown component to close when clicked outside

--- src/Components/DropDown/DropDown.jsx
+++ src/Components/DropDown/DropDown.jsx
@@ -1,28 +1,37 @@
-import React, { useState } from 'react';
-
+import React, { useRef, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
-
 import classes from './DropDown.module.css';
+import { useClickOutside } from '../../Hooks/useClickOutside';
 
-const DropDown = (props) => {
-    const [open, setOpen] = useState(false);
+const DropDown = ({ 
+    selected, 
+    setSelected, 
+    label, 
+    options, 
+    leftIcon, 
+    className, 
+    style 
+}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    useClickOutside(dropdownRef, useCallback(() => setIsOpen(false), []));
 
     const handleOpen = () => {
-        setOpen(!open);
+        setIsOpen(!isOpen);
     };
 
     const handleSelect = (key) => {
-        props.setSelected(key);
-        setOpen(false);
+        setSelected(key);
+        setIsOpen(false);
     };
 
     return (
-        <div className={`${classes.container} ${props.className}`} style={props.style}>
-            {props.label && <span>{props.label}</span>}
+        <div ref={dropdownRef} className={`${classes.container} ${className}`} style={style}>
+            {label && <span>{label}</span>}
             <button onClick={handleOpen} className={classes.dropdown}>
-                {props.leftIcon}
-                <span className={classes.dropdownText}>{props.selected}</span>
-
+                {leftIcon}
+                <span className={classes.dropdownText}>{selected}</span>
                 <div className={classes.dropdownToggle}>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -32,7 +41,7 @@ const DropDown = (props) => {
                         />
 
                         <path
-                            className={`${classes.toggleArrow} ${open ? classes.toggled : ''}`}
+                            className={`${classes.toggleArrow} ${isOpen ? classes.toggled : ''}`}
                             fillRule="evenodd"
                             clipRule="evenodd"
                             d="M9.20711 11.3894C8.76165 11.3894 8.53857 11.928 8.85355 12.243L11.6464 15.0359C11.8417 15.2311 12.1583 15.2311 12.3536 15.0358L15.1464 12.243C15.4614 11.928 15.2383 11.3894 14.7929 11.3894H9.20711Z"
@@ -41,9 +50,9 @@ const DropDown = (props) => {
                     </svg>
                 </div>
             </button>
-            {open ? (
+            {isOpen ? (
                 <ul className={classes.menu}>
-                    {props.options.map(({ option, key, icon }) => (
+                    {options.map(({ option, key, icon }) => (
                         <li className={classes['menu-item']}>
                             <button className={classes.button} onClick={() => handleSelect(key)}>
                                 {icon}
